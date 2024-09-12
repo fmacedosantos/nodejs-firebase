c5bbfe8ae72994b7ebe89b1f2e122db21b48a5d2
fmacedosantos <felipemacedo986@gmail.com>
2024-09-12 10:44:40 -0300

    Obtendo dados do firebase

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 import express, { response } from 'express';
+import admin from 'firebase-admin'
 
+// REST API
 const app = express();
+
+admin.initializeApp({
+  credential: admin.credential.cert("serviceAccountKey.json")
+});
 
 // GET
 app.get('/transactions', (request, response) => {
     console.log('GET transactions');
-    response.json([{id: 1}])
+    admin.firestore()
+    .collection('transactions')
+    .get()
+    .then(snapshot => {
+        const transactions = snapshot.docs.map(doc => ({
+            ...doc.data(),
+            uid: doc.id
+        }))
+        response.json(transactions)
+    })
 })
 
 app.listen(3000, () => console.log('API REST iniciada em http://localhost:3000'))
