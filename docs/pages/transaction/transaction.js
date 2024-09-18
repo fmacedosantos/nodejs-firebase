df6e24a833aede1e2d6c8f6a1f55a3b3d1693741
fmacedosantos <felipemacedo986@gmail.com>
2024-09-18 12:36:11 -0300

    Desenvolvendo a atualização de dados das transações

--- docs/pages/transaction/transaction.js
+++ docs/pages/transaction/transaction.js
@@ -1,8 +1,71 @@
+if(!isNewTransaction()){
+    const uid = getTransactionUid();
+    findTransactionsByUid(uid);
+}
+
+function getTransactionUid(){
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get('uid');
+}
+
+function isNewTransaction(){
+    return getTransactionUid() ? false : true;
+}
+
+function findTransactionsByUid(){
+    showLoading();
+
+    firebase.firestore()
+    .collection('transactions')
+    .doc(uid)
+    .get()
+    .then(doc => {
+        hideLoading();
+        if(doc.exists){
+            fillTransactionScreen(doc.data());
+            toggleSaveButton();
+        } else {
+            alert('Documento não encontrado');
+            window.location.href = '../home/home.html';
+        }
+    })
+    .catch(() => {
+        hideLoading();
+        alert('Erro ao recuperar documento');
+        window.location.href = '../home/home.html';
+    });
+}
+
+function fillTransactionScreen(transaction){
+    if(transaction.type == 'expense'){
+        form.typeExpense().checked = true;
+    } else {
+        form.typeIncome().checked = true;
+    }
+
+    form.date().value = transaction.date;
+    form.currency().value = transaction.money.currency;
+    form.value().value = transaction.money.value;
+    form.transactionType().value = transaction.transactionType;
+
+    if(transaction.description){
+        form.description().value = transaction.description;
+    }
+}
+
 function saveTransaction(){
     showLoading();
 
     const transaction = createTransaction();
 
+    if(isNewTransaction()){
+        save(transaction);
+    } else {
+        update(transaction);
+    }
+}
+
+function save(transaction){
     firebase.firestore()
     .collection('transactions')
     .add(transaction)
@@ -14,6 +77,23 @@ function saveTransaction(){
         hideLoading();
         alert('Erro ao salvar transação');
     })
+}
+
+function update(transaction){
+    showLoading();
+
+    firebase.firestore()
+    .collection('transactions')
+    .doc(getTransactionUid())
+    .update(transaction)
+    .then(() => {
+        hideLoading();
+        window.location.href = '../home/home.html'
+    })
+    .catch(() => {
+        hideLoading();
+        alert('Erro ao atualizar transação');
+    });
 }
 
 function createTransaction(){
@@ -83,6 +163,7 @@ function isFormValid(){
 
 const form = {
     typeExpense: () => document.getElementById('expense'),
+    typeIncome: () => document.getElementById('income'),
     date: () => document.getElementById('date'),
     dateRequiredError: () => document.getElementById('date-required-error'),
     currency: () => document.getElementById('currency'),
