a585c4cce35323f0ba571aa3e2d66098647618f0
fmacedosantos <felipemacedo986@gmail.com>
2024-09-19 18:25:08 -0300

    Separando as responsabilidades usando controllers e routers

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
-import express, { response } from 'express';
+import express from 'express';
 import admin from 'firebase-admin'
-import { authenticateToken } from './middlewares/authenticate-jwt.js';
+import { transactionRouter } from './transactions/routes.js';
 
-// REST API
 const app = express();
 
 admin.initializeApp({
   credential: admin.credential.cert("serviceAccountKey.json")
 });
 
-// GET
-app.get('/transactions', authenticateToken, (request, response) => {
-
-    admin.firestore()
-    .collection('transactions')
-    .where('user.uid', '==', request.user.uid)
-    .orderBy('date', 'desc')
-    .get()
-    .then(snapshot => {
-        const transactions = snapshot.docs.map(doc => ({
-            ...doc.data(),
-            uid: doc.id
-        }))
-        response.json(transactions)
-    })
-})
+app.use('/transactions', transactionRouter);
 
 app.listen(3000, () => console.log('API REST iniciada em http://localhost:3000'))
