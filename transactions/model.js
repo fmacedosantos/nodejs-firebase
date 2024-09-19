2f8e4cb5ac780c83ea3af0d7ceecc5681ddb351b
fmacedosantos <felipemacedo986@gmail.com>
2024-09-19 18:40:02 -0300

    Separando a lógica que envolve o banco

---
+++ transactions/model.js
@@ -0,0 +1,32 @@
+import admin from 'firebase-admin'
+
+export class Transaction {
+
+    date;
+    descripton;
+    money;
+    type;
+    user;
+
+    findByUser(){
+        if(!this.user?.uid){
+            return Promise.reject({
+                code: 500,
+                message: 'Usuário não informado'
+            })
+        }
+
+        return admin.firestore()
+            .collection('transactions')
+            .where('user.uid', '==', this.user.uid)
+            .orderBy('date', 'desc')
+            .get()
+            .then(snapshot => {
+                return snapshot.docs.map(doc => ({
+                    ...doc.data(),
+                    uid: doc.id
+                }))
+            })
+    }
+
+}
