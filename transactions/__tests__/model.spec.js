97ad73697edfabb37cdba9ff4e852440bc83588a
fmacedosantos <felipemacedo986@gmail.com>
2024-09-20 11:20:28 -0300

    testando model

--- transactions/__tests__/model.spec.js
+++ transactions/__tests__/model.spec.js
@@ -2,12 +2,43 @@ import { UserNotInformedError } from "../errors/user-not-informed-error.js";
 import { Transaction } from "../model.js"
 
 describe('Transaction model', () => {
-    test('given find user by uid, when user is not informed, then return error 500', async () => {
-      const model = new Transaction();
 
-      const response = model.findByUser();
+    const transactionRepositoryMock = {
+        findByUserUid: () => Promise.resolve([
+            {uid: 'transaction1'}, {uid: 'transaction2'}
+        ])
+    }
 
-        await expect(response).rejects.toBeInstanceOf(UserNotInformedError)
+    describe('given find user by uid', () => {
+      
+        test('when user is not informed, then return error 500', async () => {
+            const model = new Transaction();
+      
+            const response = model.findByUser();
+      
+            await expect(response).rejects.toBeInstanceOf(UserNotInformedError)
+        })
+      
+        test('when user uid is not informed, then return error 500', async () => {
+            const model = new Transaction();
+            model.user = {};
+        
+            const response = model.findByUser();
+        
+            await expect(response).rejects.toBeInstanceOf(UserNotInformedError)
+        })
+      
+        test('when user is informed, then return transaction', async () => {
+            const model = new Transaction(transactionRepositoryMock);
+            model.user = {uid: 'anyUserUid'};
+        
+            const response = model.findByUser();
+        
+            await expect(response).resolves.toEqual([
+                {uid: 'transaction1'}, {uid: 'transaction2'}
+            ])
+        })
+
     })
     
 })
